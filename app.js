6a74a98b795e019b2c3d0d8357f94fd65ae901ec
JoseJCanedo <canedo_jose@columbusstate.edu>
2022-10-08 19:26:46 +0000

    finish

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose')
+var axios = require('axios').default;
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/static', express.static("public"));
-app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 const Todo = require('./models/todo.model');
 const mongoDB = 'mongodb+srv://canedo_jose:Un6x6HqC2a1MHQCD@cluster0.pkudmic.mongodb.net/?retryWrites=true&w=majority'
@@ -14,17 +18,23 @@ db.on('error', console.error.bind(console, "MongoDb connection error: "))
 
 
 app.get('/', function(req, res){
-    Todo.find(function(err, todo){
-        console.log(todo)
-        if(err){
-            res.json({"Error: ": err})
-        } else {
-            res.render('todo.ejs', {todoList: todo});
-        }
+    axios.get('https://xkcd.com/info.0.json').then(function(response){
+        Todo.find(function(err, todo){
+            if(err){
+                res.json({"Error: ": err})
+            } else {
+                res.render('todo.ejs', {todoList: todo, comicData: response.data});
+            }
+        })
+    }).catch(function(error){
+        res.json({"Error: ": error})
     })
+    // api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
+    // min 1, max 2682 
+    
 })
 // Creates item in DB
-app.post('/', (req, res) => {
+app.post('/create', (req, res) => {
     let newTodo = new Todo({
         todo: req.body.content,
         done: false
@@ -38,10 +48,9 @@ app.post('/', (req, res) => {
     })
 })
 // Modifies item in DB
-app.put('/', (req, res) => {
+app.put('/done', (req, res) => {
     let id = req.body.id;
-    let err = {}
-    console.log(req.body)
+    let err = null
     if(typeof id === "string"){
         Todo.updateOne({_id: id}, {done: true}, function(error){
             if(error){
@@ -66,9 +75,9 @@ app.put('/', (req, res) => {
     }
 })
 
-app.delete('/', (req, res) => {
-    let id = req.body.check;
-    let err = {}
+app.delete('/delete/:id', (req, res) => {
+    let id = req.params.id;
+    let err;
     if(typeof id === "string"){
         Todo.deleteOne({_id: id}, function(error){
             if(error){
